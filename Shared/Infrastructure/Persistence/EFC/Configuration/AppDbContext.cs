7f0d3144be7fcecca6c5117f4511f55e3a199039
Vicente Quijandria <vquijandria@hotmail.com>
2025-06-20 01:24:05 -0500

    feat(shared):  added the shared functionalities

---
+++ Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace pathly_backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext
+{
+    
+}
---
+++ VocationalTest/Application/Internal/CommandServices/QuestionCommandService.cs
@@ -0,0 +1,6 @@
+﻿namespace pathly_backend.VocationalTest.Application.Internal.CommandServices;
+
+public class QuestionCommandService
+{
+    
+}
