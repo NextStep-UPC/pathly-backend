24a90129b3e6517b24b90191f6f2bfc7a040ea7a
Vicente Quijandria <vquijandria@hotmail.com>
2025-06-21 12:30:22 -0500

    feat(CareerTestDbContext):  added CareerTest DbContext

---
+++ CareerTest/Infrastructure/Persistence/Context/CareerTestDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pathly_backend.CareerTest.Domain.Model.Entities;
+
+namespace pathly_backend.CareerTest.Infrastructure.Persistence.Context
+{
+    public class CareerTestDbContext : DbContext
+    {
+        public CareerTestDbContext(DbContextOptions<CareerTestDbContext> options) : base(options) { }
+
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<QuestionOption> QuestionOptions { get; set; }
+        // Add other DbSets as needed
+    }
+}
