f9f7f2a63dda1b841d0041d56a67ecdf16dec3b0
Vicente Quijandria <vquijandria@hotmail.com>
2025-06-21 03:11:16 -0500

    feat(QuestionQueries):  coded the Question Queries layer

---
+++ CareerTest/Domain/Model/Queries/GetAllQuestionsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using pathly_backend.CareerTest.Application.Internal.DTOs;
+using System.Collections.Generic;
+
+namespace pathly_backend.CareerTest.Domain.Model.Queries;
+
+public class GetAllQuestionsQuery : IRequest<List<QuestionDto>>
+{
+}
---
+++ CareerTest/Domain/Model/Queries/GetQuestionByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using pathly_backend.CareerTest.Application.Internal.DTOs;
+
+namespace pathly_backend.CareerTest.Domain.Model.Queries;
+
+public class GetQuestionByIdQuery : IRequest<QuestionDto?>
+{
+    public int Id { get; set; }
+    public GetQuestionByIdQuery(int id) => Id = id;
+}
