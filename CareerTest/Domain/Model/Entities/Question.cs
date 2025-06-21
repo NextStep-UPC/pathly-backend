023f19b467f644dee0c20d3ce33beecfb7a37584
Vicente Quijandria <vquijandria@hotmail.com>
2025-06-21 03:12:23 -0500

    feat(Question):  coded the Question and QuestionOption Entites in Domain layer

---
+++ CareerTest/Domain/Model/Entities/Question.cs
@@ -0,0 +1,8 @@
+﻿namespace pathly_backend.CareerTest.Domain.Model.Entities;
+
+public class Question
+{
+    public int Id { get; set; }
+    public string Text { get; set; } = null!;
+    public List<QuestionOption> Options { get; set; } = new();
+}
---
+++ CareerTest/Domain/Model/Entities/QuestionOption.cs
@@ -0,0 +1,8 @@
+﻿namespace pathly_backend.CareerTest.Domain.Model.Entities;
+
+public class QuestionOption
+{
+    public int Id { get; set; }
+    public string Text { get; set; } = null!;
+    public int QuestionId { get; set; }
+}
